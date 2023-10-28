9a3fa251b46d07e3ed5041067cfc6c8757cbf3ad
Sanjay <rawatsanjay2799@gmail.com>
2023-10-28 08:22:13 +0530

    implement mongoose

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => console.log("DB connection successful!"));
+
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: [true,'A tour must have a name'],
+    unique:true,
+  },
+   rating:{
+    type:Number,
+    default:4.3
+   
+   },
+   price:{
+    type:Number,
+    require:[true,'A tour must have ']
+   }
+
+})
+const Tour = mongoose.model('Tour', tourSchema);
 const port = process.env.PORT || 3000;
-app.listen(port, () =>{
+app.listen(port, () => {
   console.log(`app runnig on port ${port}`);
 });
