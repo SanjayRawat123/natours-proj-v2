175cf9ee93f338ffb7bfa51dc8032ce2b27389da
Sanjay <rawatsanjay2799@gmail.com>
2023-11-09 08:11:39 +0530

    changes made in model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const slugify = require('slugify');
 
 const tourSchema = mongoose.Schema({
     name: {
@@ -6,17 +8,18 @@ const tourSchema = mongoose.Schema({
         required: [true, 'A tour must have a name'],
         unique: true,
     },
-    duration:{
-        type:String,
-        required:[true,'A tour must have a duration']
+    slug:String,
+    duration: {
+        type: String,
+        required: [true, 'A tour must have a duration']
     },
-    maxGroupSize:{
-        type:Number,
-        required:[true, 'A tour must have a group size']
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
     },
-    difficulty:{
-        type:String,
-        required:[true,'A tour must have difficulty']
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have difficulty']
     },
 
     ratingsAverage: {
@@ -24,35 +27,55 @@ const tourSchema = mongoose.Schema({
         default: 4.3
 
     },
-    ratingsQuantity:{
-        type:Number,
-        default:0
+    ratingsQuantity: {
+        type: Number,
+        default: 0
     },
     price: {
         type: Number,
         required: [true, 'A tour must have ']
     },
-    priceDiscount:Number,
-    summary:{
-        type:String,
-        trim:true,
-        required:[true,'Tour must have Discription']
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'Tour must have Discription']
     },
-    description:{
-        type:String,
-        trim:true
+    description: {
+        type: String,
+        trim: true
     },
-    imageCover:{
-        type:String,
-        required:[true , 'a tour must have cover image']
+    imageCover: {
+        type: String,
+        required: [true, 'a tour must have cover image']
     },
-    images:[String],
-    createdAt:{
-        type:Date,
-        default:Date.now(),
-        select:false
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        select: false
     },
-    startDates:[String]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    },
+
+},
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    }
+
+);
+tourSchema.virtual('durationWeeks').get(function () {
+    return this.duration / 7;
+});
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -5,7 +5,14 @@ const router = express.Router();
 
 // router.param('id', tourController.checkID);
 router
-     .route('/top-5-cheap').get( tourController.aliasTopTour,  tourController.getAllTours)
+    .route('/top-5-cheap').get(tourController.aliasTopTour, tourController.getAllTours);
+
+router
+    .route('/tour-stats')
+    .get(tourController.getTourStats);
+
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
 router
     .route('/')
     .get(tourController.getAllTours)
