12766d7b2ce30d7b7d9b17eb440647ddd759d42d
Sanjay <rawatsanjay2799@gmail.com>
2023-11-24 08:17:23 +0530

    implemented login funtionality and working on protecte routes

--- controller/authController.js
+++ controller/authController.js
@@ -5,41 +5,66 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
 
+
+
+const signToken = id => jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
+
+
 exports.signUp = catchAsync(async (req, res, next) => {
     const newUser = await User.create({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password,
-        passwordConfirm:req.body.passwordConfirm
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
     });
     // eslint-disable-next-line no-unused-vars
-    const token = jwt.sign({id:newUser._id},process.env.JWT_SECRET,{
-        expiresIn:process.env.JWT_EXPIRES_IN
-    })
+    const token =signToken(newUser._id)
     res.status(201).json({
         status: 'success',
         token,
-         data:{
-            user:newUser
-         }
+        data: {
+            user: newUser
+        }
     });
 
 });
 
-exports.login = (req,res,next)=>{
-  
-    const {email,password,} = req.body;
+exports.login = catchAsync(async (req, res, next) => {
+
+    const { email, password, } = req.body;
 
     // 1) check if email and password exist
-       if(!email || !password){
-        return next(new AppError('please provide email and password!',400)); 
-       }
+    if (!email || !password) {
+        return next(new AppError('please provide email and password!', 400));
+    }
     // 2) check if password correct 
-    const user = User.findOne({email})
-
+    const user = await User.findOne({ email }).select('+password')
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('Incorrect email or password', 401));
+    }
     // 3) if everthing in ok ,send token to client
-     res.status(200).json({
-        status:"success",
-         token:""
-     })
-}
+    const token = signToken(user._id)
+
+    res.status(200).json({
+        status: "success",
+        token
+    })
+});
+
+exports.protect = catchAsync(async(req,res,next)=>{
+     // 1) Getting token and checking if it is there
+     
+
+     // 2) verification token
+
+
+     //3 ) check if user exists
+
+
+     //4) check if user changed password after jwt token was issued
+     
+     
+     next()
+})
